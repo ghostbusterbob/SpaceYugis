c2b921a65459824a1c6f620db695e62c427e7232
CracksEvry <crackevry@gmail.com>
2026-09-02 11:50:59 +0200

    Player 1 + 2
    Player movement for player 1 + 2

--- Assets/scripts/Player2Movement.cs
+++ Assets/scripts/Player2Movement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class Player2Movement : MonoBehaviour
 {
     public float speed = 5f;
 
@@ -9,12 +9,12 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 move = Vector2.zero;
 
-        if (Keyboard.current.aKey.isPressed)
+        if (Keyboard.current.leftArrowKey.isPressed)
         {
             move = Vector2.left;
         }
 
-        if (Keyboard.current.dKey.isPressed)
+        if (Keyboard.current.rightArrowKey.isPressed)
         {
             move = Vector2.right;
         }
